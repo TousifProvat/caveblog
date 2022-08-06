2ae37a87afea14b80a75dbcf9ffbb1cd75e83287
Tousif Ahmed <tousifprovat@yahoo.com.au>
2022-08-06 22:26:05 +0600

    decoupled typescript types

---
+++ types/index.ts
@@ -0,0 +1,72 @@
+export interface profileTypes {
+  headline: string;
+  bio: string;
+  website: string;
+  location: string;
+}
+
+export interface userTypes {
+  username: string;
+  email: string;
+  name: string;
+  image: string;
+  Profile: profileTypes;
+}
+
+export interface postTypes {
+  id: number;
+  title: string;
+  body: string;
+  author: {
+    username: string;
+    name: string;
+    image: string;
+  };
+}
+
+export interface commentTypes {
+  id: number;
+  body: string;
+  postId: number;
+  userId: number;
+  createdAt: string;
+  updatedAt: string;
+  user: {
+    username: string;
+    name: string;
+    image: string;
+  };
+  replies: Reply[];
+}
+
+export interface Reply {
+  id: number;
+  body: string;
+  postId: number;
+  userId: number;
+  commentId: number;
+  createdAt: string;
+  updatedAt: string;
+  user: {
+    username: string;
+    name: string;
+    image: string;
+  };
+}
+
+export interface starTypes {
+  id: number;
+  post: postTypes;
+  postId: number;
+  user: userTypes;
+  userId: string;
+}
+
+export interface bookmarkTypes {
+  id: number;
+  post: postTypes;
+  postId: number;
+  user: userTypes;
+  userId: string;
+  createdAt: string;
+}
